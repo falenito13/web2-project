94b8d0cd438dda9136ff5887dc55e192ecb52a7c
falenito13 <56035351+falenito13@users.noreply.github.com>
2021-07-01 22:58:49 +0400

    web2 project

---
+++ backend/data.js
@@ -0,0 +1,37 @@
+export default   {
+    products: [{
+        _id: 1,
+        name: 'Slim shirt 1',
+        category: 'Slim',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand : 'Nike',
+        rating: 4.5,
+        countInStock: 0,
+        numReviews : 10,
+    },
+    {
+        _id: 2,
+        name: 'Fit shirt 1',
+        category: 'Shirt',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand : 'Nike',
+        countInStock: 8,
+        rating: 4.5,
+        numReviews : 10,
+    },
+    {
+        _id : 3,
+        name: 'Best shirt 1',
+        category: 'Shirtts',
+        image: '/images/d1.jpg',
+        price: 60,
+        countInStock: 10,
+        brand : 'Nike',
+        rating: 4.5,
+        numReviews : 10,
+    }
+
+]
+}
---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import data from './data.js';
+import dotenv from 'dotenv';
+import config from './config.js';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import userRoute from './routes/userRoute.js';
+import User from './models/userModel.js';
+import productRoute from './routes/productRoute.js'
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect('mongodb://localhost:27017/Ecommerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+
+app.get("/createadmin", async (req, res) => {
+  try {
+    const user = new User({
+      name: 'David',
+      email: 'd_lekveishvili@cu.edu.ge',
+      password: '1234',
+      isAdmin: true
+    });
+    const newUser = await user.save();
+    res.send(newUser);
+  } catch (error) {
+    res.send({ msg: error.message });
+  }
+});
+
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
